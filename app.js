8b8f4942d327981206edd315d38978ecfd521325
mj-ampnet <mislav@ampnet.io>
2020-03-26 21:10:57 +0100

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+var noofclicks = 0;
+
+
+
+
+function buttonClicked() {
+    if(noofclicks == 0){
+        alert("you've got corona")
+    } else {
+        alert("you're safe")
+
+    }
+    
+    noofclicks = noofclicks+1;
+}
